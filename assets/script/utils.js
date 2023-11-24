ac92397836bc42a0457a637e5cd3786324ed3961
Riley Clark <r1l3ycl4rk@gmail.com>
2023-11-24 14:41:13 -0600

    added utility functions based on given inputs

--- assets/script/utils.js
+++ assets/script/utils.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validateContactInfo(contactInfo) {
+    // Splits at commas (,)
+    const contactInfoArray = contactInfo.split(',').map((info) => info.trim());
+
+    // Checks for 3 inputs
+    if (!(contactInfoArray.length >= 3)) {
+      return false;
+    }
+  
+    const [name, city, email] = contactInfoArray;
+  
+    // Checks for empty fields
+    if (name.length === 0 || city.length === 0 || email.length === 0) {
+      return false;
+    }
+
+    // Call validateEmail()
+    validateEmail(email)
+  }
+export { validateEmail, validateContactInfo };
