4e5d4b4cfdbaf254a254bba9a64227bcfb3776fb
Riley Clark <r1l3ycl4rk@gmail.com>
2023-11-24 15:12:19 -0600

    pasted over finished code from other working doc, formatted as well

--- assets/script/Contact.js
+++ assets/script/Contact.js
@@ -1,41 +1,43 @@
-'use strict';
+"use strict";
 
 class Contact {
-    constructor(name, city, email, onDelete) {
-        this._name = name;
-        this._city = city;
-        this._email = email;
-        this._onDelete = onDelete;
-    }
+  constructor(name, city, email, onDelete) {
+    this._name = name;
+    this._city = city;
+    this._email = email;
+    this._onDelete = onDelete;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    get city() {
-        return this._city;
-    }
+  get city() {
+    return this._city;
+  }
 
-    get email() {
-        return this._email;
-    }
+  get email() {
+    return this._email;
+  }
 
-    render() {
-        const contactDiv = document.createElement('div');
-        contactDiv.classList.add('shape-grid', 'contact-item');
-        contactDiv.innerHTML = `
-            <p>Name: ${this.name}</p>
-            <p>City: ${this.city}</p>
-            <p>Email: ${this.email}</p>
+  render() {
+    const contactDiv = document.createElement("div");
+    contactDiv.classList.add("contact-grid", "contact-item");
+    contactDiv.innerHTML = `
+            <div class="contact-box">
+                <p>Name: ${this.name}</p>
+                <p>City: ${this.city}</p>
+                <p>Email: ${this.email}</p>
+            </div>
         `;
-        contactDiv.addEventListener('click', () => {
-            if (this._onDelete) {
-                this._onDelete(this);
-            }
-        });
+    contactDiv.addEventListener("click", () => {
+      if (this._onDelete) {
+        this._onDelete(this);
+      }
+    });
 
-        return contactDiv;
-    }
+    return contactDiv;
+  }
 }
 
 export { Contact };
--- assets/script/utils.js
+++ assets/script/utils.js
@@ -1,13 +1,16 @@
-'use strict';
+"use strict";
 
 function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }
 
 function validateContactInfo(contactInfo) {
-    const contactInfoArray = contactInfo.split(',').map((info) => info.trim());
-    return contactInfoArray.length === 3 && contactInfoArray.every((info) => info.length > 0);
+  const contactInfoArray = contactInfo.split(",").map((info) => info.trim());
+  return (
+    contactInfoArray.length === 3 &&
+    contactInfoArray.every((info) => info.length > 0)
+  );
 }
 
 export { validateEmail, validateContactInfo };
