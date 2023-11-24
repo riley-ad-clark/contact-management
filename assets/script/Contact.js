6a39c1f8a083264a875ceec162328d521348f809
Riley Clark <r1l3ycl4rk@gmail.com>
2023-11-24 14:34:49 -0600

    tweaked render function to fit future plans

--- assets/script/Contact.js
+++ assets/script/Contact.js
@@ -22,11 +22,13 @@ class Contact {
 
     render() {
         const contactDiv = document.createElement('div');
-        contactDiv.classList.add('shape-grid', 'contact-item'); // Add the shape-grid and contact-item classes
+        contactDiv.classList.add('contact-grid', 'contact-item');
         contactDiv.innerHTML = `
-            <p>Name: ${this.name}</p>
-            <p>City: ${this.city}</p>
-            <p>Email: ${this.email}</p>
+            <div class="contact-box">
+                <p>Name: ${this.name}</p>
+                <p>City: ${this.city}</p>
+                <p>Email: ${this.email}</p>
+            </div>
         `;
         contactDiv.addEventListener('click', () => {
             if (this._onDelete) {
